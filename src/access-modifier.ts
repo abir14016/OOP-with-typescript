2fe7fe71516302215829e3e9a04fe499189dbf27
abir14016 <abir14016@gmail.com>
2023-05-09 20:16:51 +0600

    getter and setter function

--- src/access-modifier.ts
+++ src/access-modifier.ts
@@ -1,26 +1,26 @@
-class BankAccount {
-  public id: number;
-  public name: string;
-  private _balance: number;
+// class BankAccount {
+//   public id: number;
+//   public name: string;
+//   private _balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
-  getBalance() {
-    console.log(`Current Balance ${this._balance}`);
-  }
-  deposit(amount: number) {
-    this._balance = this._balance + amount;
-  }
-}
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
+//   getBalance() {
+//     console.log(`Current Balance ${this._balance}`);
+//   }
+//   deposit(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    this.deposit;
-  }
-}
+// class StudentAccount extends BankAccount {
+//   test() {
+//     this.deposit;
+//   }
+// }
 
-const myAccount = new BankAccount(111, "Abir", 3000);
-console.log(myAccount);
+// const myAccount = new BankAccount(111, "Abir", 3000);
+// console.log(myAccount);
