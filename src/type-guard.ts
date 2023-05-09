4b8a8fc91c50e40c611412659b0cdcf68c6ed97f
abir14016 <abir14016@gmail.com>
2023-05-09 20:05:46 +0600

    access modifier

--- src/type-guard.ts
+++ src/type-guard.ts
@@ -1,94 +1,94 @@
-//keyof guard
-type Alphanumeric = number | string;
-function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
-  if (typeof param1 === "number" && typeof param2 === "number") {
-    return param1 + param2;
-  } else {
-    return param1.toString() + param2.toString();
-  }
-}
+// //keyof guard
+// type Alphanumeric = number | string;
+// function add(param1: Alphanumeric, param2: Alphanumeric): Alphanumeric {
+//   if (typeof param1 === "number" && typeof param2 === "number") {
+//     return param1 + param2;
+//   } else {
+//     return param1.toString() + param2.toString();
+//   }
+// }
 
-//in guard
+// //in guard
 
-type NormalUserType = {
-  name: string;
-};
-type AdminUserType = {
-  name: string;
-  role: "admin";
-};
+// type NormalUserType = {
+//   name: string;
+// };
+// type AdminUserType = {
+//   name: string;
+//   role: "admin";
+// };
 
-function getUser(user: NormalUserType | AdminUserType): string {
-  if ("role" in user) {
-    return `I am admin and my role is ${user.role}`;
-  } else {
-    return "I am a normal user";
-  }
-}
+// function getUser(user: NormalUserType | AdminUserType): string {
+//   if ("role" in user) {
+//     return `I am admin and my role is ${user.role}`;
+//   } else {
+//     return "I am a normal user";
+//   }
+// }
 
-const normalUser1: NormalUserType = {
-  name: "John",
-};
-const adminUser1: AdminUserType = {
-  name: "Cina",
-  role: "admin",
-};
+// const normalUser1: NormalUserType = {
+//   name: "John",
+// };
+// const adminUser1: AdminUserType = {
+//   name: "Cina",
+//   role: "admin",
+// };
 
-// console.log(getUser(normalUser1));
-// console.log(getUser(adminUser1));
+// // console.log(getUser(normalUser1));
+// // console.log(getUser(adminUser1));
 
-//instance of guard
+// //instance of guard
 
-class Animal {
-  name: string;
-  specis: string;
+// class Animal {
+//   name: string;
+//   specis: string;
 
-  constructor(name: string, specis: string) {
-    this.name = name;
-    this.specis = specis;
-  }
+//   constructor(name: string, specis: string) {
+//     this.name = name;
+//     this.specis = specis;
+//   }
 
-  makeSound() {
-    console.log("I am making sound");
-  }
-}
+//   makeSound() {
+//     console.log("I am making sound");
+//   }
+// }
 
-class Dog extends Animal {
-  constructor(name: string, specis: string) {
-    super(name, specis);
-  }
-  makeGhew() {
-    console.log("I am ghew ghew");
-  }
-}
+// class Dog extends Animal {
+//   constructor(name: string, specis: string) {
+//     super(name, specis);
+//   }
+//   makeGhew() {
+//     console.log("I am ghew ghew");
+//   }
+// }
 
-class Cat extends Animal {
-  constructor(name: string, specis: string) {
-    super(name, specis);
-  }
+// class Cat extends Animal {
+//   constructor(name: string, specis: string) {
+//     super(name, specis);
+//   }
 
-  makeMeaw() {
-    console.log("I am meaw meaw");
-  }
-}
+//   makeMeaw() {
+//     console.log("I am meaw meaw");
+//   }
+// }
 
-function isDog(animal: Animal): animal is Dog {
-  //is syntax
-  return animal instanceof Dog;
-}
+// function isDog(animal: Animal): animal is Dog {
+//   //is syntax
+//   return animal instanceof Dog;
+// }
 
-function getAnimal(animal: Animal) {
-  if (isDog(animal)) {
-    animal.makeGhew();
-  } else if (animal instanceof Cat) {
-    animal.makeMeaw();
-  } else {
-    animal.makeSound();
-  }
-}
+// function getAnimal(animal: Animal) {
+//   if (isDog(animal)) {
+//     animal.makeGhew();
+//   } else if (animal instanceof Cat) {
+//     animal.makeMeaw();
+//   } else {
+//     animal.makeSound();
+//   }
+// }
 
-//instance of dog
-const animal1 = new Dog("Bruno", "dog");
-const animal2 = new Cat("Guddu", "cat");
+// //instance of dog
+// const animal1 = new Dog("Bruno", "dog");
+// const animal2 = new Cat("Guddu", "cat");
 
-getAnimal(animal2);
+// getAnimal(animal2);
